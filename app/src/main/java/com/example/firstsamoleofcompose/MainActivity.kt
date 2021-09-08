b21c82ab74f35aa953179f488574ec527bd195c3
kaaveh <kaaveh@pm.me>
2021-09-08 08:10:46 +0430

    Finish #1 example

--- app/src/main/java/com/example/firstsamoleofcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstsamoleofcompose/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.firstsamoleofcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstsamoleofcompose.ui.theme.FirstSamoleOfComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +33,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val context = LocalContext.current
+    val name = rememberSaveable{ mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(value = name.value,
+            onValueChange = {
+                name.value = it
+            })
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                Toast.makeText(context, "Hi ${name.value}!", Toast.LENGTH_SHORT).show()
+            }) {
+            Text(text = "Click me!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
